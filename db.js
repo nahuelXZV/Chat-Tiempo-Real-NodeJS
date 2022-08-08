195fde02bbd1a75a01c450652df863e61ed0ae73
Nahuel Zalazar <zvnahuel63@gmail.com>
2022-08-08 18:27:30 -0400

    ...

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const db = require('mongoose');
 db.Promise = global.Promise; 
 
 async function connect() {
-    await db.connect('mongodb+srv://nahuel:Fireware123@nahuelxzv.8vcvv0p.mongodb.net/?retryWrites=true&w=majority', {
+    await db.connect('', {
         useNewUrlParser: true,
     }).then(() => {
         console.log('[db] Conectada con éxito');
