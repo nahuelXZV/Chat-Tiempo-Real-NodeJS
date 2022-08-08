3ea8f3f0ea4ab732b95440633ac705af9e327264
Nahuel Zalazar <zvnahuel63@gmail.com>
2022-08-08 18:20:39 -0400

    finish backend

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 const express = require('express');
+const app = express(); // Create express app
+const server = require('http').Server(app); // Create http server
+
+const socket = require('./socket');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const router = require('./network/routes');
 const db = require('./db');
 
-db(); // Connect to database
 
-const app = express(); // Create express app
+
+socket.connect(server); // Connect socket to server
+db(); // Connect to database
 app.use(bodyParser.json()); // Parse JSON body
+app.use(cors()); // Allow CORS
 router(app);        // Register routes
 
-app.use('/app', express.static('public')); 
-
-app.listen(3000, function () {
+app.use('/app', express.static('public'));
+server.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 });
